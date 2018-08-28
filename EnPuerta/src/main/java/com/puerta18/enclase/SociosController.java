27f346d7c97846feab9d77c37d08373e345f9b06
backend18 <backend18@p18-profes02.puerta18.corp.ar>
2018-08-28 10:48:21 -0300

    first commit

---
+++ EnPuerta/src/main/java/com/puerta18/enclase/SociosController.java
@@ -0,0 +1,49 @@
+package com.puerta18.enclase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SociosController {
+
+	@Autowired
+	private Environment env;
+
+	// landing page, muestra un formulario de busqueda 
+	// y tambien muestra los resultados con un parametro no requerido 
+	@GetMapping("/")
+	public String landing() {
+		System.out.println(env.getProperty("spring.datasource.password"));
+		return "";
+	}
+	
+	@GetMapping("/socios/nuevo") // formulario de alta vacio
+	public String nuevo() {
+		return "";
+	}
+	
+	@GetMapping("/socios/nuevo/procesar") // inserta nuevos socios
+	public String insertarNuevo() {
+		return "";
+	}
+	
+	@GetMapping("/socios/checkin/{id}") // 
+	public String checkIn() {
+		return "";
+	}
+	
+	@GetMapping("/socios/checkout/{id}") // 
+	public String checkOut() {
+		//dni=dni.replaceAll("\\.", ""); 
+		return "";
+	}
+	
+	// estas rutas mas adelante vamos a protegerlas con usuario y contraseña
+	// @GetMapping("/socios/mostrar/{id}") // muestra el detalle completo de un socio
+	// @GetMapping("/socios/listado")      // muestra el listado completo sin paginacion, por ahora
+	
+	// @GetMapping("/socios/modificar/{id}")
+	// @GetMapping("/socios/modificar/procesar/{id}")
+}
